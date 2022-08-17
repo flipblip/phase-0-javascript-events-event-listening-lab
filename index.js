61a0556fadff1c7f0cb686c5d4eacd8aef299873
wickynjy@gmail.com <meow@meow>
2022-08-17 13:08:22 +0300

    Done with Assignment

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
+// const input = document.getElementById('button')
+
+// function buttonClicked() {
+//     alert('button clicked');
+// }
+
+// input.addEventListener('click', buttonClicked)
+
+
+
+// const input = document.getElementById('button');
+// input.addEventListener('click', function() {
+//   alert('I was clicked!');
+// });
+
+
+// // function addingEventListener(){
+
+// input.addEventListener('click', function() {
+//     alert('I was clicked');
+// });
+const input = document.getElementById('button');
+
 function addingEventListener() {
+    input.addEventListener('click', addingEventListener);
 }
+
+
